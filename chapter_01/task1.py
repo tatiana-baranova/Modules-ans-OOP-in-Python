91dc21525ed0325c707ecd4f8358b579151cb69c
tetiana_baranova <syniovych.tanya@gmail.com>
2026-03-09 20:36:03 +0200

    modules

---
+++ chapter_01/task1.py
@@ -0,0 +1,12 @@
+import math
+import time
+import platform
+
+print(platform.platform())
+print(platform.system())
+print(platform.release())
+
+time.sleep(3)
+
+res = math.pow(math.pi, 5)
+print(round(res))
