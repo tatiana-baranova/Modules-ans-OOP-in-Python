7505e3c3e2bac5bfa9c57c6881fb5b9d733570ee
tetiana_baranova <syniovych.tanya@gmail.com>
2026-03-12 12:59:37 +0200

    independent work with list enum

--- chapter_02/task2.py
+++ chapter_02/task2.py
@@ -1,15 +1,28 @@
 from enum import Enum
 
 
-class Choices(Enum):
-    Sweet = 1,
-    Spice = 2,
-    Ice = 3,
-    Hot = 4
+# class Choices(Enum):
+#     Sweet = 1,
+#     Spice = 2,
+#     Ice = 3,
+#     Hot = 4
 
-user_choice = Choices.Sweet
-if(user_choice == Choices.Sweet):
-    print('User selected sweets')
+# user_choice = Choices.Sweet
+# if(user_choice == Choices.Sweet):
+#     print('User selected sweets')
 
-if(user_choice == Choices.Hot):
-    print('User selected hot')
+# if(user_choice == Choices.Hot):
+#     print('User selected hot')
+
+
+class DayOfWeek(Enum):
+    Monday = 1,
+    Tuesday = 2,
+    Wednesday = 3,
+    Thursday = 4,
+    Friday = 5,
+    Saturday = 6,
+    Sunday = 7
+
+for day in DayOfWeek:
+    print(day)
