38377176dba9a1dd6bd24f7a356471a42f7672a6
tetiana_baranova <syniovych.tanya@gmail.com>
2026-03-12 13:35:20 +0200

    independent work with multithrea

--- chapter_02/task3.py
+++ chapter_02/task3.py
@@ -1,29 +1,37 @@
 import time
 import threading
 
-def sleepTime(wait, name):
-    print("Enter text: {}".format(name))
-    time.sleep(wait)
-    print("Enter text repeatedly: {}".format(name))
+# def sleepTime(wait, name):
+#     print("Enter text: {}".format(name))
+#     time.sleep(wait)
+#     print("Enter text repeatedly: {}".format(name))
+
+# start = time.time()
+# t_list = []
+# for i in range(5):
+#     name = 'SleepTime ' + str(i + 1)
+#     print('{} start'.format(name) )
+#     td = threading.Thread(target=sleepTime, name=name, args=(3, name))
+#     td.start()
+#     t_list.append(td)
+
+# for t in t_list:
+#     t.join()
+
+# end = time.time()
+
+# print("Time: ", end - start)
+
+def print_numbers():
+    for i in range(1, 6):
+        print(f"Number: {i}")
+        time.sleep(1)
+    
 
 
-# td.start()
-# td.join()
-start = time.time()
-t_list = []
-for i in range(5):
-    name = 'SleepTime ' + str(i + 1)
-    print('{} start'.format(name) )
-    td = threading.Thread(target=sleepTime, name=name, args=(3, name))
-    td.start()
-    t_list.append(td)
+thread = threading.Thread(target=print_numbers)
 
-for t in t_list:
-    t.join()
+thread.start()
+thread.join()
 
-# for e in range(5):
-#     sleepTime(3, e)
-
-end = time.time()
-
-print("Time: ", end - start)
+print('Головний потік завершив роботу')
